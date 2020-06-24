444ede2b7d1f0832ccc3df807eca2d4b735b9405
urosgluscevic <56654298+urosgluscevic@users.noreply.github.com>
2020-06-24 15:33:21 +0200

    added mongodb

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const DB_URL = "mongodb://localhost:27017/book-sale";
+const DB_URL = process.env.MONGODB_URI ||"mongodb://urosmiodrag:urosmiodrag123@ds039331.mlab.com:39331/heroku_v97n9qzj";
 const PORT = process.env.PORT || 5000;
 
 module.exports = {
