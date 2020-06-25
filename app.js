da1554dabd549d61e94617e4157e153a61f414fd
MiodragKruscic <miokruscic@gmail.com>
2020-06-25 22:46:24 +0200

    sa

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(json());
 app.use(fileupload());
 
 app.get("/", (req, res) => {
-    res.status(200).json({"Message":"Welcome to book api"});
+    res.status(200).json({"Message":"Welcome to boosk api"});
 })
 
 
