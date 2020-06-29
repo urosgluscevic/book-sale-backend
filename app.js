27902122134e7c70f8301affdeae2c4a14a3cfaf
urosgluscevic <56654298+urosgluscevic@users.noreply.github.com>
2020-06-30 01:24:24 +0200

    upload image test

--- app.js
+++ app.js
@@ -398,7 +398,10 @@ app.post("/uploadImage/:uploadTo", verifyToken, (req, res)=>{ //images will be s
         if (err){
             res.sendStatus(401);
         } else{
-            const file = req.files.upfile;
+            // const file = req.files.upfile;
+            const file = req.body.upfile;
+            console.log(req.body)
+            console.log({file});
             const name = file.name;
             const uploadPath = __dirname + "/" + name;
 
